6f50230e0cd6f8b300cd505c312236db96ebc575
z <zees-dev@users.noreply.github.com>
2025-02-18 11:08:09 +1300

    updated llm.js example to use hardcoded supported model

--- examples/llm.js
+++ examples/llm.js
@@ -1,11 +1,17 @@
+const supportedModels = JSON.stringify(MODELS, null, 2);
+console.log("Supported Models", supportedModels);
 
 // Create instance
-const llm = BlessLLM("Llama-3.1-8B-Instruct-q4f32_1-MLC");
+const llm = BlessLLM(MODELS.MISTRAL_7B.DEFAULT);
 
 // Set options
 llm.setOptions({
   system_message: "You are a helpful assistant. First time I ask, your name will be Lucy. Second time I ask, your name will be Bob."
 });
+
+// Get options
+const options = llm.getOptions();
+console.log("Options", JSON.stringify(options, null, 2));
 
 // Chat
 console.log(llm.chat("What is your name?"));
