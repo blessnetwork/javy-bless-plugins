f398563cb2d7bf4faf1042e8f1b5dfed24bd079e
Joinhack <joinhack@gmail.com>
2025-05-20 14:37:07 +0800

    close support

---
+++ src/wasi/close.rs
@@ -0,0 +1,28 @@
+use javy_plugin_api::javy::{
+    quickjs::{Object as JObject, Value}, 
+    Args
+};
+use anyhow::{anyhow, bail, Result};
+
+use super::{preview_1, set_error};
+
+/// This function is used to close a file descriptor.
+pub fn wasi_preview1_close<'a>(args: Args<'a>) -> Result<Value<'a>> {
+    let (cx, args) = args.release();
+    let args_pat: &[Value<'_>]= &args.0;
+    let [
+        fd,
+        ..
+    ] =  args_pat else {
+        bail!(
+            "close expects 1 parameter: the fd, Got: {} parameters.",
+            args.len()
+        );
+    };
+    let fd = fd.as_int()
+        .ok_or_else(|| anyhow!("fd must be a number"))?;
+    let rs = unsafe { preview_1::fd_close(fd) };
+    let rs_obj = JObject::new(cx.clone())?;
+    set_error(&rs_obj, rs)?;
+    Ok(Value::from_object(rs_obj))
+}
