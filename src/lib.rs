cbe9b4bfdaccd0adc8d89518a78f73f21809009e
z <zees-dev@users.noreply.github.com>
2025-07-29 16:26:23 +1200

    refactor modules to make them importable with external runtimes

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
+#[allow(unused_imports)]
 use javy_plugin_api::{
     import_namespace,
     javy::{
         hold, hold_and_release,
-        quickjs::{prelude::MutFn, Function},
+        quickjs::{prelude::MutFn, Ctx, Function, Result},
         to_js_error, Args,
     },
     Config,
@@ -28,94 +29,19 @@ pub extern "C" fn initialize_runtime() {
     config.javy_stream_io(true);
     config.text_encoding(true);
 
+    #[allow(unused_variables)]
     javy_plugin_api::initialize_runtime(config, |runtime| {
         runtime
             .context()
             .with(|ctx| {
                 #[cfg(feature = "crypto")]
-                ctx.globals().set(
-                    "__javy_crypto_get_random_values",
-                    Function::new(
-                        ctx.clone(),
-                        MutFn::new(move |cx, args| {
-                            let (cx, args) = hold_and_release!(cx, args);
-                            bless_get_random_values(hold!(cx.clone(), args))
-                                .map_err(|e| to_js_error(cx, e))
-                        }),
-                    )?,
-                )?;
-
+                set_crypto_globals(&ctx)?;
                 #[cfg(feature = "fetch")]
-                ctx.globals().set(
-                    "__javy_fetchio_request",
-                    Function::new(
-                        ctx.clone(),
-                        MutFn::new(move |cx, args| {
-                            let (cx, args) = hold_and_release!(cx, args);
-                            bless_fetch_request(hold!(cx.clone(), args))
-                                .map_err(|e| to_js_error(cx, e))
-                        }),
-                    )?,
-                )?;
-
+                set_fetch_globals(&ctx)?;
                 #[cfg(feature = "wasip1")]
-                {
-                    macro_rules! bind {
-                        (function, $l: ident) => {
-                            let name = concat!("__javy_", stringify!($l));
-                            ctx.globals().set(
-                                name,
-                                Function::new(
-                                    ctx.clone(),
-                                    MutFn::new(move |cx, args| {
-                                        let (cx, args) = hold_and_release!(cx, args);
-                                        wasi::$l(hold!(cx.clone(), args))
-                                            .map_err(|e| to_js_error(cx, e))
-                                    }),
-                                )?,
-                            )?;
-                        };
-                    }
-                    bind!(function, wasi_preview1_open);
-                    bind!(function, wasi_preview1_fd_prestat_dir_name);
-                    bind!(function, wasi_preview1_path_create_directory);
-                    bind!(function, wasi_preview1_path_remove_directory);
-                    bind!(function, wasi_preview1_path_unlink_file);
-                    bind!(function, wasi_preview1_close);
-                    bind!(function, wasi_preview1_path_symlink);
-                    bind!(function, wasi_preview1_path_link);
-                    bind!(function, wasi_preview1_path_rename);
-                    bind!(function, wasi_preview1_path_filestat_get);
-                }
-
+                set_wasi_globals(&ctx)?;
                 #[cfg(feature = "llm")]
-                ctx.globals().set(
-                    "BlessLLM",
-                    Function::new(
-                        ctx.clone(),
-                        MutFn::new(move |cx, args| {
-                            let (cx, args) = hold_and_release!(cx, args);
-                            bless_llm_plugin(hold!(cx.clone(), args))
-                                .map_err(|e| to_js_error(cx, e))
-                        }),
-                    )?,
-                )?;
-
-                // Expose the suppported models object globally for JS
-                #[cfg(feature = "llm")]
-                ctx.globals().set(
-                    "MODELS",
-                    javy_plugin_api::javy::quickjs::Value::from_object(
-                        llm::supported_models_object(&ctx)?,
-                    ),
-                )?;
-
-                #[cfg(feature = "crypto")]
-                ctx.eval::<(), _>(include_str!("crypto/crypto.js"))?;
-                #[cfg(feature = "fetch")]
-                ctx.eval::<(), _>(include_str!("fetch/fetch.js"))?;
-                #[cfg(feature = "wasip1")]
-                ctx.eval::<(), _>(include_str!("wasi/preview_1.js"))?;
+                set_llm_globals(&ctx)?;
                 Ok::<_, anyhow::Error>(())
             })
             .unwrap();
@@ -123,4 +49,92 @@ pub extern "C" fn initialize_runtime() {
         runtime
     })
     .unwrap();
+}
+
+#[cfg(feature = "crypto")]
+pub fn set_crypto_globals(ctx: &Ctx<'_>) -> Result<()> {
+    ctx.globals().set(
+        "__javy_crypto_get_random_values",
+        Function::new(
+            ctx.clone(),
+            MutFn::new(move |cx, args| {
+                let (cx, args) = hold_and_release!(cx, args);
+                crypto::bless_get_random_values(hold!(cx.clone(), args))
+                    .map_err(|e| to_js_error(cx, e))
+            }),
+        )?,
+    )?;
+    ctx.eval::<(), _>(include_str!("crypto/crypto.js"))?;
+    Ok(())
+}
+
+#[cfg(feature = "fetch")]
+pub fn set_fetch_globals(ctx: &Ctx<'_>) -> Result<()> {
+    ctx.globals().set(
+        "__javy_fetchio_request",
+        Function::new(
+            ctx.clone(),
+            MutFn::new(move |cx, args| {
+                let (cx, args) = hold_and_release!(cx, args);
+                fetch::bless_fetch_request(hold!(cx.clone(), args)).map_err(|e| to_js_error(cx, e))
+            }),
+        )?,
+    )?;
+    ctx.eval::<(), _>(include_str!("fetch/fetch.js"))?;
+    Ok(())
+}
+
+#[cfg(feature = "wasip1")]
+pub fn set_wasi_globals(ctx: &Ctx<'_>) -> Result<()> {
+    macro_rules! bind {
+        (function, $l: ident) => {
+            let name = concat!("__javy_", stringify!($l));
+            ctx.globals().set(
+                name,
+                Function::new(
+                    ctx.clone(),
+                    MutFn::new(move |cx, args| {
+                        let (cx, args) = hold_and_release!(cx, args);
+                        wasi::$l(hold!(cx.clone(), args)).map_err(|e| to_js_error(cx, e))
+                    }),
+                )?,
+            )?;
+        };
+    }
+    bind!(function, wasi_preview1_open);
+    bind!(function, wasi_preview1_fd_prestat_dir_name);
+    bind!(function, wasi_preview1_path_create_directory);
+    bind!(function, wasi_preview1_path_remove_directory);
+    bind!(function, wasi_preview1_path_unlink_file);
+    bind!(function, wasi_preview1_close);
+    bind!(function, wasi_preview1_path_symlink);
+    bind!(function, wasi_preview1_path_link);
+    bind!(function, wasi_preview1_path_rename);
+    bind!(function, wasi_preview1_path_filestat_get);
+    ctx.eval::<(), _>(include_str!("wasi/preview_1.js"))?;
+    Ok(())
+}
+
+#[cfg(feature = "llm")]
+pub fn set_llm_globals(ctx: &Ctx<'_>) -> Result<()> {
+    ctx.globals().set(
+        "BlessLLM",
+        Function::new(
+            ctx.clone(),
+            MutFn::new(move |cx, args| {
+                let (cx, args) = hold_and_release!(cx, args);
+                llm::bless_llm_plugin(hold!(cx.clone(), args)).map_err(|e| to_js_error(cx, e))
+            }),
+        )?,
+    )?;
+
+    // Expose the suppported models object globally for JS
+    let ctx_clone = ctx.clone();
+    ctx.globals().set(
+        "MODELS",
+        javy_plugin_api::javy::quickjs::Value::from_object(
+            llm::supported_models_object(ctx).map_err(|e| to_js_error(ctx_clone, e))?,
+        ),
+    )?;
+    Ok(())
 }
